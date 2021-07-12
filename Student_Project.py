893ac961a01bb8961c10df6962a23494562f392a
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-07-12 12:29:55 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,21 @@
+#Import module
+#Import module
+
+def #Write the function name():
+    #Take user input for radius
+    #Take user input for color
+    turtle.penup()
+    turtle.fillcolor(#Pass the variable color)
+    turtle.goto(random.randint(#Provide the range)+20,random.randint(#Provide the range)+20)
+    #Begin with the color fill
+    turtle.circle(#Fill the value)
+    #End the color fill
+    turtle.pendown()
+    turtle.pencolor("black")
+    turtle.goto(0,-200)
+    turtle.stamp()
+
+#Call the function
+#Call the function
+#Call the function
+#Call the function
